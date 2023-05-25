9a931a77994baa89820276c9b2f3e1b232b6f7d9
Brandon Jacobson <bjacobson891@gmail.com>
2023-05-25 10:58:21 -0700

    Co-authored-by: bxviu <bxviu@users.noreply.github.com> Co-authored-by: joshwidjaja <joshwidjaja@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # S7RapidPrototyping
+A prototype showcasing the flow of scenes in the S7: Rapid Prototyping assignment  
+
+# Group Members
+Brandon Jacobson  
+Benthan Vu  
+Joshua Widjaja  
+
+# Deployment Link
+https://undeadkyuubi.github.io/S7RapidPrototyping/  
---
+++ sceneFlow.js
@@ -0,0 +1,164 @@
+
+class Stage extends Phaser.Scene {
+    constructor() {
+        super("stage")
+    }
+    
+    init(data) {
+        this.currentLevel = data.level;
+    }
+
+    create() {
+        // player presses right to simulate win, left to simulate loss
+        let up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        let left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+
+        if (this.currentLevel == 1) {
+            this.add.text(200, 400, "Watch out for slugs!");
+        }
+        else if (this.currentLevel == 2) {
+            this.add.text(200, 400, "Watch out for snails!");
+        }
+        else {
+            this.add.text(200, 400, "This is the last level!");
+        }
+        up.on('down', () => {
+            this.scene.start('levelclear', {level: this.currentLevel + 1});
+        });
+        
+        left.on('down', () => {
+            this.scene.start('gameover')
+        });
+        let level = this.add.text(200, 100, "current level " + this.currentLevel);
+        let gameplay = this.add.text(200, 300, "gameplay screen\npress left to lose; up to win");
+    }
+
+    update() {
+        
+    }
+}
+
+class GameOver extends Phaser.Scene {
+    constructor() {
+        super("gameover")
+    }
+
+    create() {
+        this.add.text(400,300,"Game Over")
+    }
+
+    update() {
+        
+    }
+}
+
+class LevelClear extends Phaser.Scene {
+    constructor() {
+        super("levelclear")
+    }
+
+    init(data) {
+        this.currentLevel = data.level;
+    }
+
+    create() {
+        if (this.currentLevel == 2) {
+            let text = this.add.text(200, 300, "That's good work!");
+            this.input.on('pointerdown', () => {
+                this.tweens.add({
+                    targets: text,
+                    x: 1000,
+                    duration: 500,
+                    ease: 'Cubic.in',
+                    onComplete: () => {
+                        this.time.delayedCall(250, () => {
+                            this.scene.start('stage', {level:2})
+                        });
+                    }
+                });
+            });
+        }
+        else if (this.currentLevel == 3) {
+            this.add.text(200, 300, "Good job!");
+            this.input.on('pointerdown', () => {
+                this.tweens.add({
+                    targets: text,
+                    x: 1000,
+                    duration: 500,
+                    ease: 'Cubic.in',
+                    onComplete: () => {
+                        this.time.delayedCall(250, () => {
+                            this.scene.start('stage' , {level:3})
+                        });
+                    }
+                });
+            });
+        }
+        else {
+            this.add.text(200, 300, "The roly poly gets a badge!");
+            this.input.on('pointerdown', () => {
+                this.tweens.add({
+                    targets: text,
+                    x: 1000,
+                    duration: 500,
+                    ease: 'Cubic.in',
+                    onComplete: () => {
+                        this.time.delayedCall(250, () => {
+                            this.scene.start('winscreen')
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    update() {
+        
+    }
+}
+
+class WinScreen extends Phaser.Scene {
+    constructor() {
+        super("winscreen")
+    }
+
+    create() {
+        this.add.text(400,300,"You Win")
+        this.add.text(400,400,"click to replay");
+        this.input.on('pointerdown', () => this.scene.start('titlescreen'));
+    }
+
+    update() {
+        
+    }
+}
+
+class TitleScreen extends Phaser.Scene {
+    constructor() {
+        super("titlescreen")
+    }
+
+    create() {
+        this.add.text(400,300,"Roly Poly: To the End");
+        this.add.text(400,400,"click to start");
+        this.input.on('pointerdown', () => this.scene.start('stage', {level:1}));
+    }
+
+    update() {
+        
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: [TitleScreen, Stage, GameOver, LevelClear, WinScreen],
+};
+
+var game = new Phaser.Game(config);
